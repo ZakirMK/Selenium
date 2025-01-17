d81cc7da26dda759a6093de0eddb6e027a6d8512
Zakir Mustafa Kemal Kalayci <118366212+ZakirMK@users.noreply.github.com>
2025-01-17 17:39:53 +0300

    remove pytest

--- pytest_project/utils/helper.py
+++
@@ -1,9 +0,0 @@
-from pages.login_page import LoginPage
-from config import Globals
-
-def login(driver):
-    """Helper function to log in."""
-    login_page = LoginPage(driver)
-    login_page.enter_username(Globals.VALID_USERNAME)
-    login_page.enter_password(Globals.VALID_PASSWORD)
-    login_page.click_login()
